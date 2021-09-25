b77b41a495b7a0a2fbd882c6a6e382e7b6135a90
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-25 22:48:03 +0530

    Added: Using user id generate invoice summary and fetch rides list

--- src/main/java/InvoiceService.java
+++ src/main/java/InvoiceService.java
@@ -1,20 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class InvoiceService {
-    private static final double DISTANCE_KM_FARE_RATE = 10.0, TIME_MIN_FARE_RATE = 1, MINIMUM_FARE = 5;
+    private static final double DISTANCE_KM_FARE_RATE = 10, TIME_MIN_FARE_RATE = 1, MINIMUM_FARE = 5;
+    private static ArrayList<Ride> ridesList;
+    private static HashMap<String, ArrayList<Ride>> userRidesMap;
+
+    InvoiceService(){
+        ridesList = new ArrayList<>();
+        userRidesMap = new HashMap<>();
+    }
 
     public double calculateFare(Ride ride) {
         double totalFare = ride.getDistance().getDistanceInKm() * DISTANCE_KM_FARE_RATE + ride.getTime().getTimeInMin() * TIME_MIN_FARE_RATE;
         return Math.max(totalFare, MINIMUM_FARE);
     }
 
-    public double getTotalAggregateFare(Ride[] rideList) {
+
+    public double getTotalAggregateFare(String userId) {
         double totalAggregateFare = 0;
-        for (Ride ride : rideList) {
+        for (Ride ride : userRidesMap.get(userId)) {
             totalAggregateFare += calculateFare(ride);
         }
         return totalAggregateFare;
     }
 
-    public String getSummary(Ride[] rideList) {
-        return "Total number of rides: " + rideList.length + "\nTotal fare amount: " + getTotalAggregateFare(rideList) + "\nAverage Fare per ride: " + (getTotalAggregateFare(rideList) / rideList.length);
+    public String getSummary(String userId) {
+
+        double totalLength = userRidesMap.get(userId).size();
+        return "Total number of rides: " + totalLength + "\nTotal fare amount: " + getTotalAggregateFare(userId) + "\nAverage Fare per ride: " + (getTotalAggregateFare(userId) / totalLength);
+    }
+
+    public void addRides(String userId, Ride ride) {
+        if (userRidesMap.containsKey(userId)) {
+            ArrayList<Ride> rideList = userRidesMap.get(userId);
+            rideList.add(ride);
+        } else {
+            ridesList.add(ride);
+            userRidesMap.put(userId, ridesList);
+        }
+    }
+
+    public String showRideList(String userId) {
+        StringBuilder showRideDetails = new StringBuilder();
+        showRideDetails.append("User Id:").append(userId);
+        for (Ride ride : userRidesMap.get(userId)) {
+            showRideDetails.append("\nDistance: ");
+            showRideDetails.append(ride.getDistance().getDistanceInKm());
+            showRideDetails.append("\tTime: ");
+            showRideDetails.append(ride.getTime().getTimeInMin());
+        }
+        return showRideDetails.toString();
+    }
+
+    public HashMap<String, ArrayList<Ride>> getUserRidesMap() {
+        return userRidesMap;
     }
 }
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -2,12 +2,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class InvoiceServiceTest {
     static InvoiceService invoice;
+    static ArrayList<Ride> rideList;
 
     @BeforeEach
     public void setUp() {
         invoice = new InvoiceService();
+        rideList = new ArrayList<>();
     }
 
     @Test
@@ -26,29 +30,48 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenMultipleRides_shouldReturnAggregateTotalFare() {
-        Ride[] rideList = new Ride[]{
-                new Ride(new MeasuredUnit(Unit.KM, 2), new MeasuredUnit(Unit.MIN, 5)),
-                new Ride(new MeasuredUnit(Unit.METER, 500.0), new MeasuredUnit(Unit.MIN, 3)),
-                new Ride(new MeasuredUnit(Unit.METER, 100.0), new MeasuredUnit(Unit.MIN, 1.0))
-        };
-        double totalAggregateFare = invoice.getTotalAggregateFare(rideList);
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.KM, 2.0), new MeasuredUnit(Unit.MIN, 5.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 500.0), new MeasuredUnit(Unit.MIN, 3.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 200.0), new MeasuredUnit(Unit.MIN, 1.0)));
+        double totalAggregateFare = invoice.getTotalAggregateFare("1");
         double expectedResult = 38.0;
-        Assertions.assertEquals(expectedResult, totalAggregateFare);
+        Assertions.assertEquals(expectedResult, totalAggregateFare,0.01);
     }
 
     @Test
     public void givenMultipleRides_shouldReturnInvoiceSummary() {
-        Ride[] rideList = new Ride[]{
-                new Ride(new MeasuredUnit(Unit.KM, 2), new MeasuredUnit(Unit.MIN, 5)),
-                new Ride(new MeasuredUnit(Unit.METER, 500.0), new MeasuredUnit(Unit.MIN, 3)),
-                new Ride(new MeasuredUnit(Unit.METER, 100.0), new MeasuredUnit(Unit.MIN, 1.0)),
-                new Ride(new MeasuredUnit(Unit.METER, 200.0), new MeasuredUnit(Unit.MIN, 4))
-        };
-        String invoiceSummary = invoice.getSummary(rideList);
-        int noOfRides = 4;
-        double totalAggregateFare = 44.0, averageFarePerRide = 11;
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.KM, 2.0), new MeasuredUnit(Unit.MIN, 5.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 500.0), new MeasuredUnit(Unit.MIN, 3.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 200.0), new MeasuredUnit(Unit.MIN, 1.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 200.0), new MeasuredUnit(Unit.MIN, 4.0)));
+        String invoiceSummary = invoice.getSummary("1");
+        double noOfRides = 4.0, totalAggregateFare = 44.0, averageFarePerRide = 11.0;
         String expectedSummary = "Total number of rides: " + noOfRides + "\nTotal fare amount: " + totalAggregateFare + "\nAverage Fare per ride: " + averageFarePerRide;
         Assertions.assertEquals(expectedSummary, invoiceSummary);
     }
 
+    @Test
+    public void givenUserId_shouldReturnRideListAndInvoiceSummary() {
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.KM, 2.0), new MeasuredUnit(Unit.MIN, 5.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 500.0), new MeasuredUnit(Unit.MIN, 3.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 200.0), new MeasuredUnit(Unit.MIN, 1.0)));
+        invoice.addRides("1",new Ride(new MeasuredUnit(Unit.METER, 200.0), new MeasuredUnit(Unit.MIN, 4.0)));
+        // rides list
+        String ridesList = invoice.showRideList("1");
+        StringBuilder expectedRidesList = new StringBuilder();
+        expectedRidesList.append("User Id:").append("1");
+        for (Ride ride : invoice.getUserRidesMap().get("1")) {
+            expectedRidesList.append("\nDistance: ");
+            expectedRidesList.append(ride.getDistance().getDistanceInKm());
+            expectedRidesList.append("\tTime: ");
+            expectedRidesList.append(ride.getTime().getTimeInMin());
+        }
+        Assertions.assertEquals(expectedRidesList.toString(), ridesList);
+
+        // invoice summary
+        String invoiceSummary = invoice.getSummary("1");
+        double noOfRides = 4.0, totalAggregateFare = 44.0, averageFarePerRide = 11.0;
+        String expectedSummary = "Total number of rides: " + noOfRides + "\nTotal fare amount: " + totalAggregateFare + "\nAverage Fare per ride: " + averageFarePerRide;
+        Assertions.assertEquals(expectedSummary, invoiceSummary);
+    }
 }
