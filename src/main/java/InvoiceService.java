9a9649b87667a0027f6d30470069e227ad93a327
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-25 16:14:45 +0530

    Added: Calculate fare using distance and time

--- src/main/java/InvoiceService.java
+++ src/main/java/InvoiceService.java
@@ -1,3 +1,8 @@
 public class InvoiceService {
+    private static final double DISTANCE_KM_FARE_RATE = 10.0, TIME_MIN_FARE_RATE = 1, MINIMUM_FARE = 5;
 
+    public double calculateFare(double distanceKm, int timeMin) {
+        double totalFare = distanceKm*DISTANCE_KM_FARE_RATE+timeMin* TIME_MIN_FARE_RATE;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
 }
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,3 +1,22 @@
-public class InvoiceServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class InvoiceServiceTest {
+    @Test
+    public void given2KmAnd5Min_shouldReturn25TotalFare(){
+        InvoiceService invoice = new InvoiceService();
+        double distanceKm = 2.0;
+        int timeMin = 5;
+        double fare = invoice.calculateFare(distanceKm,timeMin);
+        Assertions.assertEquals(25,fare);
+    }
+
+    @Test
+    public void given100MeterAnd1Min_shouldReturnMinimumFare(){
+        InvoiceService invoice = new InvoiceService();
+        double distanceKm = 0.1; // meter in Km (conversion)
+        int timeMin = 1;
+        double fare = invoice.calculateFare(distanceKm,timeMin);
+        Assertions.assertEquals(5,fare);
+    }
 }
